a7190b77ce6e64d5e4eeda27ccfcb0b13a814e4c
qChoices <brightedemgawu7@gmail.com>
2022-07-08 08:19:41 +0000

    aa

--- 0x04-more_functions_nested_loops/3-main.c
+++
@@ -1,8 +0,0 @@
-#include "main.h"
-
-
-int main(void)
-{
-    print_numbers();
-    return (0);
-}
--- 0x04-more_functions_nested_loops/4-main.c
+++
@@ -1,9 +0,0 @@
-#include "main.h"
-
-
-void print_most_numbers(void);
-int main(void)
-{
-    print_most_numbers();
-    return (0);
-}
